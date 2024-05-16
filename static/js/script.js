b59d20c299ff85a5cd6801032a6e596cf5929203
Maximiliam999 <maximanuel777@gmail.com>
2024-05-16 15:55:09 +0000

    import fonts and add to homepage, new image for homepage

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,10 @@
-console.log("message")
+window.addEventListener('scroll', function(){
+    var navbar = document.querySelector('.navbar');
+    var scrollPosition = window.scrollY;
+
+    if (scrollPosition > 100) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled')
+    }
+})
