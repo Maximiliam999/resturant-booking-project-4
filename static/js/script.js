fbf6fa9f357d026c9b98c06473b999f23b1301b2
Maximiliam999 <maximanuel777@gmail.com>
2024-06-11 15:58:33 +0000

    add slider for promises both js and css

--- static/js/script.js
+++ static/js/script.js
@@ -1,10 +1,32 @@
-window.addEventListener('scroll', function(){
-    var navbar = document.querySelector('.navbar');
-    var scrollPosition = window.scrollY;
+let slidesCarousel = document.getElementById('promises-slideshow');
+let slidesList = document.getElementById('slides-list')
+let slides = document.querySelectorAll('.my-slides');
+let next = document.getElementById('next-arrow');
+let prev = document.getElementById('prev-arrow');
 
-    if (scrollPosition > 100) {
-        navbar.classList.add('scrolled');
+let currentSlide = 0;
+
+updateSlide();
+
+next.addEventListener('click', () => {
+    if(currentSlide < slides.length -1) {
+        currentSlide++;
     } else {
-        navbar.classList.remove('scrolled')
+        currentSlide = 0;
     }
-})
+    updateSlide()
+});
+
+prev.addEventListener('click', () => {
+    if (currentSlide > 0 ) {
+        currentSlide--;
+    } else {
+        currentSlide = slides.length - 1
+    }
+    updateSlide()
+});
+
+function updateSlide() {
+    let slideWidth = slides[0].clientWidth;
+    slidesList.style.transform = `translateX(-${currentSlide * slideWidth}px)`
+}
